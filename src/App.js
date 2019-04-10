cc90d9b6bedc69a6b354df15026f71cda9ee6e0b
MFAC <ninafroes@gmail.com>
2019-04-10 09:59:18 +0200

    feat: Add getWeather method

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import Form from './components/Form';
 import Weather from './components/Weather';
 
 const API_KEY = 'f6d3ccc3be4411b42258113ccd95bfec';
+const CITY_ID = '524901';
 
 class App extends React.Component {
+  
+  getWeather = async () => {
+    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/forecast?id=${CITY_ID}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  } 
+  
   render() {
     return (
       <div>
