c661ac7a588d437b931df835f14b7e61bd47e371
MFAC <ninafroes@gmail.com>
2019-04-10 13:36:39 +0200

    refactor: Turn stateless components into arrow functions

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-class Titles extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <p>{this.props.text}</p>
-      </div>
-    );
-  }
-}
+const Titles = ({ title, text }) => (
+  <div>
+    <h1>{title}</h1>
+    <p>{text}</p>
+  </div>
+)
 
 export default Titles;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-class Weather extends React.Component {
-  render() {
-    return (
-      <div>
-        {  this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>  }
-        {  this.props.temperature && <p>Temperature: {this.props.temperature}°C</p>  }
-        {  this.props.humidity && <p>Humidity: {this.props.humidity}% </p>  }
-        {  this.props.description && <p>Conditions: {this.props.description}</p>  }
-        {  this.props.error && <p>{this.props.error}</p>  }
-      </div>
-    );
-  }
-}
+const Weather = ({ city, country, temperature, humidity, description, error}) => (
+    <div>
+      {  city && country && <p>Location: {city}, {country}</p>  }
+      {  temperature && <p>Temperature: {temperature}°C</p>  }
+      {  humidity && <p>Humidity: {humidity}% </p>  }
+      {  description && <p>Conditions: {description}</p>  }
+      {  error && <p>{error}</p>  }
+    </div>
+);
 
 export default Weather;
