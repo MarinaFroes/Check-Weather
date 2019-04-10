53e89f857a58aa3a8eaf5bf48ee2b3818745e439
MFAC <ninafroes@gmail.com>
2019-04-10 10:14:07 +0200

    feat: Add form element

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,11 @@ import React from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <div>Form component</div>
+      <form>
+        <input type="text" name="city" placeholder="City..."/>
+        <input type="text" name="country" placeholder="Country..." />
+        <input type="submit" value="Get Weather"/>
+      </form>
     );
   }
 }
