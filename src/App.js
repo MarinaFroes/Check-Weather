4ac94261b07be65de34880b10790b74b58d02cc4
MFAC <ninafroes@gmail.com>
2019-04-10 09:26:11 +0200

    feat: Add Form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Titles from './components/Titles';
+import Form from './components/Form';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
           title="Weather Checker"
           text="Find out temperature, conditions and more..."
         />
+        <Form />
       </div>
     );
   }
