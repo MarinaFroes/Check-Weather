93056695da9d316d8c01635c76515431bc235646
MFAC <ninafroes@gmail.com>
2019-04-16 20:33:36 +0200

    Updates

---
+++ precache-manifest.efa8977ca5cb87b321e62f3cfbcf6d21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcb68e086e961cf2f6fd",
+    "url": "/Check-Weather/static/css/main.a08051e3.chunk.css"
+  },
+  {
+    "revision": "bcb68e086e961cf2f6fd",
+    "url": "/Check-Weather/static/js/main.1c099fab.chunk.js"
+  },
+  {
+    "revision": "beb20d44f170746a67ab",
+    "url": "/Check-Weather/static/js/runtime~main.30538c19.js"
+  },
+  {
+    "revision": "35330e86450f047ac964",
+    "url": "/Check-Weather/static/js/2.0a66c6f9.chunk.js"
+  },
+  {
+    "revision": "23e4a95725d33d32d608433086112972",
+    "url": "/Check-Weather/index.html"
+  }
+];
